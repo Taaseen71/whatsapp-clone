7e78002259bdccafc62d92d613bbf29c4b9ca38c
Saadat Taaseen <taaseen.homework@gmail.com>
2020-09-08 16:38:01 -0400

    connected backend with front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Sidebar from './Components/Sidebar';
 import Chat from './Components/Chat';
+import Pusher from 'pusher-js';
 
 function App() {
+
+    useEffect(() => {
+        const pusher = new Pusher('446a5868e105ded7d86d', {
+            cluster: 'us2'
+        });
+
+        const channel = pusher.subscribe('messages');
+        channel.bind('inserted', function (data) {
+            alert(JSON.stringify(data));
+        });
+
+
+    }, [])
+
     return (
         <div className="app">
             <h1>WhatsApp</h1>
---
+++ client/src/Pusher.js
@@ -0,0 +1,10 @@
+
+// const pusher = new Pusher('446a5868e105ded7d86d', {
+//     cluster: 'us2'
+// });
+
+// const channel = pusher.subscribe('my-channel');
+// channel.bind('my-event', function (data) {
+//     alert(JSON.stringify(data));
+// });
+
